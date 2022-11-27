c8ccef2b4f94efce07ea3fc206d5e204d86da601
Chewan Rai <105234638+chewanrai@users.noreply.github.com>
2022-11-27 16:44:53 +0545

    addbook gui and database added

---
+++ src/controller/Addbookcontroller.java
@@ -0,0 +1,51 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package controller;
+//
+///**
+// *
+// * @author raich
+// */
+//public class Addbookcontroller {
+//    
+//}
+
+package controller;
+//import Database.Dbconnection;
+import database.DbConnection;
+import model.Addbook;
+//import models.Student;
+public class Addbookcontroller{
+    DbConnection dbconnection;
+
+    public int insertbook(Addbook book){
+        int book_id = book.getBook_id();
+        String book_name = book.getBook_name();
+        String author = book.getAuthor();
+        String genre = book.getGenre();
+        String pdf_link = book.getPdf_link();
+        String more = book.getMore();
+        String insertQuery = String.format ("INSERT INTO Addbook VAlUES(%d,'%s', '%s', '%s', '%s', '%s')",book_id,book_name,author, genre,pdf_link, more);
+        System.out.println(insertQuery);
+        dbconnection = new DbConnection();
+        int result = dbconnection.manipulate(insertQuery); // manipulate in datavase
+        return result;
+    }
+
+    public int updatebook(Addbook book){
+        int book_id = book.getBook_id();
+        String book_name = book.getBook_name();
+        String author = book.getAuthor();
+        String genre = book.getGenre();
+        String pdf_link = book.getPdf_link();
+        String more = book.getMore();
+        String updateQuery = String.format ("UPDATE tbl_student SET where book_id=%d, book_name='%s', author= '%s', genre='%s', pdf_link= '%s', more= '%s' ",book_id,book_name,author, genre,pdf_link, more);
+        dbconnection = new DbConnection();
+        int result = dbconnection.manipulate(updateQuery);
+        return result;
+    }
+
+    
+}
---
+++ src/model/Addbook.java
@@ -0,0 +1,104 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package models;
+//
+///**
+// *
+// * @author raich
+// */
+//public class Addbook {
+//    
+//}
+
+
+package model;
+
+public class Addbook {
+
+  private int book_id;
+  private String book_name;
+  private String author;
+  private String genre;
+  private String pdf_link;
+  private String more;
+
+   // creation of public constructor
+    public Addbook(
+    int book_id,
+    String book_name,
+    String author,
+    String genre,
+    String pdf_link,
+    String more
+  ) {
+    this.book_id = book_id;
+    this.book_name = book_name;
+    this.author = author;
+    this.genre = genre;
+    this.pdf_link = pdf_link;
+    this.more = more;
+  }
+    public Addbook(
+    String book_name,
+    String author,
+    String genre,
+    String pdf_link,
+    String more
+  ) {
+    this.book_name = book_name;
+    this.author = author;
+    this.genre = genre;
+    this.pdf_link = pdf_link;
+    this.more = more;
+  }
+
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    public String getBook_name() {
+        return book_name;
+    }
+
+    public void setBook_name(String book_name) {
+        this.book_name = book_name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPdf_link() {
+        return pdf_link;
+    }
+
+    public void setPdf_link(String pdf_link) {
+        this.pdf_link = pdf_link;
+    }
+
+    public String getMore() {
+        return more;
+    }
+
+    public void setMore(String more) {
+        this.more = more;
+    }
+}
