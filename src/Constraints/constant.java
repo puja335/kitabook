03e629387996ea30fc7c5bf546b10b485a47e8c0
Ansaldude <ansal.subedi@gmail.com>
2022-12-16 10:11:40 +0545

    dash

---
+++ src/Constraints/constant.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Constraints;
+import model.Addbook;
+/**
+ *
+ * @author Dell
+ */
+public class constant {
+    public static String DB_BOOK_ID = "book_id"; 
+    public static String DB_BOOK_NAME = "book_name";
+    public static String DB_AUTHOR = "author";
+    public static String DB_GENRE = "genre";
+    public static String DB_PDF_LINK = "pdf_link";
+    public static String DB_MORE = "more";
+    public static Addbook Books;
+}
--- src/controller/UsersController.java
+++ src/controller/UsersController.java
@@ -7,92 +7,10 @@ import database.DbConnection;
 import model.*;
 import java.sql.*;
 import javax.swing.JOptionPane;
-/**
- *
- * @author puja
- */
+
 public class UsersController {
       DbConnection dbconnection;
-        public int updateUsers(String fname,String lname, String username, String phonenum){
-//        String firstname = users.getFirstName();
-//        String last_name = users.getLastName();
-//        String phonenum = users.getPhoneNumber();
-//        String Usernamee = users.getUsername();
-        String updateQuery = String.format("UPDATE users SET first_name = '%s', lastname = '%s' , username = '%s' WHERE phonenumber = '%s'",fname,lname,username ,phonenum);
-        dbconnection = new DbConnection();
-        int result = dbconnection.manipulate(updateQuery);
-        return result;
-    }
-        
-        public Boolean loginUser(String username, String password) {
-    String selectQuery = String.format(
-      "select * from users where username = '%s' and userpassword = '%s' ",
-      username,
-      password
-    );
-    
-          DbConnection dbConnection = new DbConnection();
-    ResultSet rs = dbConnection.retrieve(selectQuery);
-    System.out.println(rs);
-    try {
-      while (rs.next()) {
-        String fetchedUserName = rs.getString("username");
-        String fetchedPassword = rs.getString("userpassword");
-        System.out.println(fetchedUserName + fetchedPassword);
-        if (username.equals(fetchedUserName) && password.equals(fetchedPassword)) {
-          return true;
-        }
-      }
-    } catch (Exception e) {
-      // TODO: handle exception
-      System.out.println("Error");
-      return false;
-    }
-    return false;
-  }
-        
-        public int updatepass(String passw, String pnumber){
-        String updateQuery = String.format("UPDATE users SET userpassword = '%s' WHERE phonenumber = '%s'", passw, pnumber);
-        dbconnection = new DbConnection();
-        int result = dbconnection.manipulate(updateQuery);
-        return result;
-    }
-        public int validatePhone(String pnumb){
-            
-    String selectQuery = String.format(
-      "select phonenumber from users ");
-                  DbConnection dbConnection = new DbConnection();
-                   ResultSet rs = dbConnection.retrieve(selectQuery);
-                       try {
-      while (rs.next()) {
-        String fetchednumber = rs.getString("phonenumber");
-        if (pnumb.equals(fetchednumber)) {
-          return 1;
-        }
-        else{
-            JOptionPane.showMessageDialog(null,"Enter valid number.");
-        return 0;
-        }
-      }
-    } catch (Exception e) {
-      // TODO: handle exception
-      System.out.println("Error");
-      return 0;
-    }
-      return 0;
-        }
-        public int viewprofile(String fname,String lname,String usern,String pnumber){
-            String selectQuery = String.format(
-      "select * from users where username = '%s'",
-      
-      usern,
-      
-    );
-            DbConnection dbConnection = new DbConnection();
-                   ResultSet rs = dbConnection.retrieve(selectQuery);
-        }
-    
-      
+
       public int registerUser(Users users){
         String firstname = users.getFirstName();
         String last_name = users.getLastName();
@@ -101,11 +19,14 @@ public class UsersController {
         String password = users.getPassword();
         String cPassword = users.getconf_Password();
         dbconnection = new DbConnection();
-        String insertQuery = String.format("INSERT INTO users(first_name,lastname,phonenumber,username,userpassword,confpassword) VALUES('%s','%s','%s','%s','%s','%s')",firstname,last_name,phonenum,username,password,cPassword);
-          System.out.println(insertQuery);
+        String insertQuery = String.format("INSERT INTO Users(first_name,lastname,phonenumber,username,userpassword,confpassword) VALUES('%s','%s','%s','%s','%s','%s')",firstname,last_name,phonenum,username,password,cPassword);
+        System.out.println(insertQuery);
         int result = dbconnection.manipulate(insertQuery);
         return result;
       }
+      
+       
        
 }
+
 
