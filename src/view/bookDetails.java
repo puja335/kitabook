e3b95d4681e7d620458afd7a13fc81500dd60e6a
Chewan Rai <105234638+chewanrai@users.noreply.github.com>
2022-12-28 18:41:00 +0545

    library btn of bookdetails

--- src/view/bookDetails.java
+++ src/view/bookDetails.java
@@ -244,7 +244,8 @@ public class bookDetails extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-
+        new library().setVisible(true);
+        this.setVisible(false);      
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
