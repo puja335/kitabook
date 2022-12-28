26d91a31097fbbb8e298f469b6772be61def7477
Chewan Rai <105234638+chewanrai@users.noreply.github.com>
2022-12-28 19:03:55 +0545

    Comming soon trending fucntion

--- src/view/commingsoon.java
+++ src/view/commingsoon.java
@@ -84,6 +84,11 @@ public class commingsoon extends javax.swing.JFrame {
         jButton3.setBorder(null);
         jButton3.setBorderPainted(false);
         jButton3.setContentAreaFilled(false);
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setBackground(new java.awt.Color(251, 211, 174));
         jButton4.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
@@ -237,6 +242,12 @@ public class commingsoon extends javax.swing.JFrame {
         new dashboard().setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        new TrendingScreen().setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
