da4c0cbb8dc8ba0f19acb0211a90cf99bf5c8ad0
Chewan Rai <105234638+chewanrai@users.noreply.github.com>
2022-12-28 18:38:35 +0545

    Home btn function in bookdetails

--- src/view/bookDetails.java
+++ src/view/bookDetails.java
@@ -238,6 +238,8 @@ public class bookDetails extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        new dashboard().setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
